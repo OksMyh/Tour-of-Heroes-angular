df99567def7aba37c1bb8849f698dd6b69099736
Oksana <oks.mykhailiuk@gmail.com>
2019-02-11 19:15:19 +0200

    Routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,8 +13,14 @@ export class HeroService {
 
   constructor( private  messageService: MessageService) { }
 
-    getHeroes (): Observable<Hero []> {
-    this.messageService.add('HeroService: fetched heroes')
-    return of (HEROUS);
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROUS);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROUS.find(hero => hero.id === id));
   }
 }
